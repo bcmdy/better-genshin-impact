3a9ca8e4e3010cfa0a9d6f31fe24a4c4fb54a877
茶包 <kaedelcb@qq.com>
2025-07-22 03:57:48 +0800

    优化1

--- BetterGenshinImpact/GameTask/Common/Job/NetworkRecovery.cs
+++ BetterGenshinImpact/GameTask/Common/Job/NetworkRecovery.cs
@@ -1,85 +1,19 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
-using BetterGenshinImpact.Core.Simulator;
-using BetterGenshinImpact.GameTask.Common.BgiVision;
-using Vanara.PInvoke;
 using static BetterGenshinImpact.GameTask.Common.TaskControl;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-using BetterGenshinImpact.Core.Simulator;
-using BetterGenshinImpact.GameTask.AutoGeniusInvokation.Exception;
-using BetterGenshinImpact.GameTask.AutoWood.Assets;
-using BetterGenshinImpact.GameTask.AutoWood.Utils;
-using BetterGenshinImpact.GameTask.Common.BgiVision;
 using BetterGenshinImpact.GameTask.Common.Element.Assets;
 using BetterGenshinImpact.GameTask.Model.Area;
-using Microsoft.Extensions.Logging;
-using Vanara.PInvoke;
-using static BetterGenshinImpact.GameTask.Common.TaskControl;
-
-using BetterGenshinImpact.Core.Config;
-using BetterGenshinImpact.Core.Recognition.OCR;
-using BetterGenshinImpact.Core.Recognition.ONNX;
-using BetterGenshinImpact.Core.Simulator;
-using BetterGenshinImpact.Core.Simulator.Extensions;
 using BetterGenshinImpact.GameTask.AutoFight.Assets;
-using BetterGenshinImpact.GameTask.AutoFight.Model;
-using BetterGenshinImpact.GameTask.AutoFight.Script;
-using BetterGenshinImpact.GameTask.AutoGeniusInvokation.Exception;
-using BetterGenshinImpact.GameTask.AutoPick.Assets;
-using BetterGenshinImpact.GameTask.Common.Map;
-using BetterGenshinImpact.GameTask.Model.Area;
-using BetterGenshinImpact.Helpers;
-using BetterGenshinImpact.Service.Notification;
-using BetterGenshinImpact.View.Drawable;
-using Microsoft.Extensions.Logging;
-using OpenCvSharp;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing.Imaging;
-using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using BetterGenshinImpact.Core.Recognition;
-using BetterGenshinImpact.GameTask.AutoTrackPath;
-using BetterGenshinImpact.GameTask.Common.BgiVision;
-using BetterGenshinImpact.GameTask.Common.Element.Assets;
-using BetterGenshinImpact.GameTask.Common.Job;
-using BetterGenshinImpact.Service.Notification.Model.Enum;
-using Vanara.PInvoke;
-using static BetterGenshinImpact.GameTask.Common.TaskControl;
-using static Vanara.PInvoke.Kernel32;
-using static Vanara.PInvoke.User32;
-using Microsoft.Extensions.Localization;
-using System.Globalization;
 using System.Text.RegularExpressions;
-using BetterGenshinImpact.GameTask.AutoArtifactSalvage;
-using System.Collections.ObjectModel;
-using BetterGenshinImpact.Core.Script.Dependence;
-using BetterGenshinImpact.GameTask.AutoDomain.Model;
-using BetterGenshinImpact.GameTask.Common;
-using Compunet.YoloSharp;
-using Microsoft.Extensions.DependencyInjection;
+
 
 namespace BetterGenshinImpact.GameTask.Common.Job;
 
 public class NetworkRecovery
 {
     public string Name => "断网恢复";
-
-
-    private readonly BgiYoloPredictor _predictor;
-
-    private readonly CombatScriptBag _combatScriptBag;
-
-    private CancellationToken _ct;
-
-    private ObservableCollection<OneDragonFlowConfig> ConfigList = [];
-    
-    private readonly ReturnMainUiTask _returnMainUiTask = new();
     
     private static RecognitionObject GetConfirmRa(bool isOcrMatch = false,params string[] targetText)
     {
@@ -115,8 +49,7 @@ public class NetworkRecovery
             1000
         );
         
-        //等待3秒
-        await Task.Delay(3000);
+        await Task.Delay(3000, ct);
         
         await NewRetry.WaitForElementDisappear(
             GetConfirmRa(true,"连接超时","连接已断开","网络错误","无法登录服务器","提示","通知"),
@@ -147,7 +80,6 @@ public class NetworkRecovery
                     Regex.IsMatch(t.Text, "确认"));
                 if (enterDone != null)
                 {
-                    Logger.LogWarning("确认");
                     enterDone.Click();
                     enterDone.Dispose();
                 }
