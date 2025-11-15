8356097b830079073272b587ec32d6bf674a40a9
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2025-11-16 00:55:51 +0800

    fix: 修复配置组设置无法调整高度 (#2468)

--- BetterGenshinImpact/ViewModel/Pages/ScriptControlViewModel.cs
+++ BetterGenshinImpact/ViewModel/Pages/ScriptControlViewModel.cs
@@ -347,6 +347,7 @@ public partial class ScriptControlViewModel : ViewModel
             Owner = Application.Current.MainWindow,
             WindowStartupLocation = WindowStartupLocation.CenterOwner,
         };
+        uiMessageBox.SourceInitialized += (s, e) => WindowHelper.TryApplySystemBackdrop(uiMessageBox);
 
         void OnQuestionButtonOnClick(object sender, RoutedEventArgs args)
         {
@@ -2178,8 +2179,11 @@ public partial class ScriptControlViewModel : ViewModel
         {
             Title = "配置组设置",
             Content = new ScriptGroupConfigView(new ScriptGroupConfigViewModel(TaskContext.Instance().Config, SelectedScriptGroup.Config)),
-            SizeToContent = SizeToContent.WidthAndHeight,
-            ResizeMode = ResizeMode.NoResize,
+            Width = 800,
+            Height = 600,
+            MinWidth = 800,
+            MaxWidth = 800,
+            MinHeight = 600,
             WindowStartupLocation = WindowStartupLocation.CenterOwner,
             ExtendsContentIntoTitleBar = true,
             WindowBackdropType = WindowBackdropType.Auto,
