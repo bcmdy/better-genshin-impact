1b770100afc306b0c446e764cf80c087716ba615
kaedelcb <kaedelcb@qq.com>
2026-03-07 18:29:14 +0800

    feat(js):地图追踪增加读取目录下的内容等方法 (#2875)
    * feat(js):地图追踪增加读取目录下的内容等方法
    
    * refactor(AutoPathingScript): 重构文件操作方法到LimitedFile类
    
    * fix
    
    * docs(AutoPathingScript): 修正方法参数注释中的默认值描述
    
    * 避免重复初始化实例，给恢复游戏窗口焦点的日志也加上当前窗口名

--- BetterGenshinImpact/Core/Script/Dependence/AutoPathingScript.cs
+++ BetterGenshinImpact/Core/Script/Dependence/AutoPathingScript.cs
@@ -86,4 +86,37 @@ public class AutoPathingScript
         var json = await AutoPathingFile.ReadText(path);
         await Run(json);
     }
+
+    /// <summary>
+    /// 判断 AutoPathing 目录下的路径是否存在
+    /// </summary>
+    /// <param name="subPath">相对于 User\AutoPathing 的路径</param>
+    /// <returns>存在返回 true，否则返回 false</returns>
+    public bool IsExists(string subPath) => AutoPathingFile.IsExists(subPath);
+
+    /// <summary>
+    /// 判断 AutoPathing 目录下的路径是否为文件
+    /// </summary>
+    /// <param name="subPath">相对于 User\AutoPathing 的路径</param>
+    /// <returns>是文件返回 true，否则返回 false</returns>
+    public bool IsFile(string subPath) => AutoPathingFile.IsFile(subPath);
+
+    /// <summary>
+    /// 判断 AutoPathing 目录下的路径是否为文件夹
+    /// </summary>
+    /// <param name="subPath">相对于 User\AutoPathing 的路径</param>
+    /// <returns>是文件夹返回 true，否则返回 false</returns>
+    public bool IsFolder(string subPath) => AutoPathingFile.IsFolder(subPath);
+
+    /// <summary>
+    /// 读取 AutoPathing 目录下指定文件夹的内容（非递归方式）
+    /// </summary>
+    /// <param name="subPath">相对于 User\AutoPathing 的子目录路径，默认为相对根目录</param>
+    /// <returns>文件夹内所有文件和文件夹的相对路径数组，出错时返回空数组</returns>
+    public string[] ReadPathSync(string subPath = "./") => AutoPathingFile.ReadPathSync(subPath);
+
+    /// <summary>
+    /// LimitedFile 实例，用于操作 AutoPathing 目录
+    /// </summary>
+    private LimitedFile AutoPathingFile => _autoPathingFile;
 }
