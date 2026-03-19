f04153e8ef281b18facd6808e99c1aa7f044fa6a
茶包 <kaedelcb@qq.com>
2026-03-19 20:54:26 +0800

    化减等待/2K吃药

--- BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
+++ BetterGenshinImpact/GameTask/AutoPathing/PathExecutor.cs
@@ -2099,6 +2099,7 @@ public class PathExecutor
                 // 自动疾跑
                 if (distance > 20 && PartyConfig.AutoRunEnabled)
                 {
+                    // var dashTime = nextDistance > 90 ? 3500 : 2500;
                     if (Math.Abs((fastModeColdTime - DateTime.UtcNow).TotalMilliseconds) > 2500) //冷却时间2.5s，回复体力用
                     {
                         fastModeColdTime = DateTime.UtcNow;
@@ -2260,7 +2261,7 @@ public class PathExecutor
                 SuccessFight++;
             }
 
-            if (PartyConfig.QuicklySkip && (_lastWaypoint?.Action == ActionEnum.Fight.Code || waypoint.Action == ActionEnum.Fight.Code))
+            if (PartyConfig.QuicklySkip && (_lastWaypoint?.Action == ActionEnum.Fight.Code || waypoint.Action == ActionEnum.Fight.Code || nextWaypoint?.Action == ActionEnum.Fight.Code))
             {
                 if (nextWaypoint?.Type != WaypointType.Teleport.Code)
                 {
