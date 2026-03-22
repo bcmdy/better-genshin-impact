bd35cedccee442ca86b34460d30feaf033c90b97
辉鸭蛋 <huiyadanli@gmail.com>
2026-03-23 02:31:14 +0800

    自动烹饪修改成独立任务 (#2949)

--- BetterGenshinImpact/Core/Config/AllConfig.cs
+++ BetterGenshinImpact/Core/Config/AllConfig.cs
@@ -1,5 +1,4 @@
 using BetterGenshinImpact.GameTask;
-using BetterGenshinImpact.GameTask.AutoCook;
 using BetterGenshinImpact.GameTask.AutoDomain;
 using BetterGenshinImpact.GameTask.AutoFight;
 using BetterGenshinImpact.GameTask.AutoFishing;
@@ -24,6 +23,7 @@ using BetterGenshinImpact.GameTask.AutoStygianOnslaught;
 using BetterGenshinImpact.GameTask.GetGridIcons;
 using BetterGenshinImpact.GameTask.AutoEat;
 using BetterGenshinImpact.GameTask.AutoLeyLineOutcrop;
+using BetterGenshinImpact.GameTask.AutoCook;
 using BetterGenshinImpact.GameTask.MapMask;
 using BetterGenshinImpact.GameTask.SkillCd;
 using BetterGenshinImpact.GameTask.UseRedeemCode;
@@ -177,11 +177,6 @@ public partial class AllConfig : ObservableObject
     public QuickTeleportConfig QuickTeleportConfig { get; set; } = new();
 
     /// <summary>
-    ///     自动烹饪配置
-    /// </summary>
-    public AutoCookConfig AutoCookConfig { get; set; } = new();
-
-    /// <summary>
     ///     自动打牌配置
     /// </summary>
     public AutoGeniusInvokationConfig AutoGeniusInvokationConfig { get; set; } = new();
@@ -228,6 +223,8 @@ public partial class AllConfig : ObservableObject
     ///     自动地脉花配置
     /// </summary>
     public AutoLeyLineOutcropConfig AutoLeyLineOutcropConfig { get; set; } = new();
+
+    public AutoCookConfig AutoCookConfig { get; set; } = new();
     
     /// <summary>
     ///   地图遮罩
@@ -318,7 +315,6 @@ public partial class AllConfig : ObservableObject
         AutoSkipConfig.PropertyChanged += OnAnyPropertyChanged;
         AutoFishingConfig.PropertyChanged += OnAnyPropertyChanged;
         QuickTeleportConfig.PropertyChanged += OnAnyPropertyChanged;
-        AutoCookConfig.PropertyChanged += OnAnyPropertyChanged;
         MacroConfig.PropertyChanged += OnAnyPropertyChanged;
         HotKeyConfig.PropertyChanged += OnAnyPropertyChanged;
         AutoWoodConfig.PropertyChanged += OnAnyPropertyChanged;
@@ -329,6 +325,7 @@ public partial class AllConfig : ObservableObject
         AutoRedeemCodeConfig.PropertyChanged += OnAnyPropertyChanged;
         AutoEatConfig.PropertyChanged += OnAnyPropertyChanged;
         AutoLeyLineOutcropConfig.PropertyChanged += OnAnyPropertyChanged;
+        AutoCookConfig.PropertyChanged += OnAnyPropertyChanged;
         MapMaskConfig.PropertyChanged += OnAnyPropertyChanged;
         AutoMusicGameConfig.PropertyChanged += OnAnyPropertyChanged;
         TpConfig.PropertyChanged += OnAnyPropertyChanged;
